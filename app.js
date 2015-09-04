49f83c0e0845ef6ce2088b2ec94a5c2c914f5144
Ar1k <arunkmr492@gmail.com>
2015-09-04 17:48:08 +0530

    build updated for heroku

---
+++ app.js
@@ -0,0 +1,63 @@
+var express =require('express');
+  var express = require('express'),
+    app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+// listen for new web clients:
+server.listen(5000);
+
+//requests coming for desktop
+app.get('/', function(req, res){
+   res.sendfile(__dirname + '/public/index.html');
+});
+
+
+//requests coming for mobile
+app.get('/p1/:id', function(req, res){
+   res.sendfile(__dirname + '/public/p1.html');
+});
+
+app.get('/p2/:id', function(req, res){
+   res.sendfile(__dirname + '/public/p1.html');
+});
+
+
+// handle rest of the requests
+app.use(express.static(__dirname + '/public'));
+
+//------------------- express routes ends--------------------------------------//
+
+//Socket functions starts//
+io.sockets.on('connection', function (socket) {
+
+  socket.on('new_room', function(data){
+		socket.join(data);
+	});
+
+  socket.on('add', function(data) {
+    room = data.roomId;
+		socket.broadcast.to(room).emit('fire', data);
+		//io.sockets.emit('fire', data);
+	});
+
+	socket.on('begin', function(data){
+		room = data.roomId;
+		socket.broadcast.to(room).emit('started', data);
+    //io.sockets.emit('started');
+	});
+
+	socket.on('remove', function(data) {
+    room = data.roomId;
+    socket.broadcast.to(room).emit('remove_particle', data);
+    //io.sockets.emit('remove_particle');
+	});
+
+	socket.on('add_name', function(data){
+    room = data.roomId;
+    socket.broadcast.to(room).emit('add_name1', data);
+    //io.sockets.emit('add_name1', data);
+		console.log(data);
+	});
+});
