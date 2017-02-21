faa6239258e50b10ab468d0fa438d93e7229b189
Arun Kumaran <arunkmr492@gmail.com>
2017-02-21 15:49:55 +0530

    remote url added

--- public/assets/utils.js
+++ public/assets/utils.js
@@ -1,7 +1,8 @@
 var common = {
   baseUrl : {
     connect : function(){
-      return "192.168.1.173:3000"
+      //return "192.168.1.173:3000"
+      return "https://quick-draw.herokuapp.com/"
     }
   }
 }
