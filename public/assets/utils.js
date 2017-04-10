ce237a2eba99bada9b8238f74e7a591883b05f64
Arun Kumaran <arunkmr492@gmail.com>
2017-04-10 18:24:36 +0530

    updated instructions

--- public/assets/utils.js
+++ public/assets/utils.js
@@ -1,8 +1,8 @@
 var common = {
   baseUrl : {
     connect : function(){
-      //return "192.168.2.207:3000"
-      return "https://quick-draw.herokuapp.com/"
+      return "192.168.2.206:3000"
+      //return "https://quick-draw.herokuapp.com/"
     }
   }
 }
