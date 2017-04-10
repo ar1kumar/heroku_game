5fba2c010a9060ca82487c37081c1b2573668672
Arun Kumaran <arunkmr492@gmail.com>
2017-04-10 16:55:36 +0530

    fixed some stupid bugs. This is build for someone really special, I hope you like it.

--- public/assets/utils.js
+++ public/assets/utils.js
@@ -1,7 +1,7 @@
 var common = {
   baseUrl : {
     connect : function(){
-      //return "192.168.1.173:3000"
+      //return "192.168.2.207:3000"
       return "https://quick-draw.herokuapp.com/"
     }
   }
